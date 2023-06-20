aa9d29f091cb4a5be3d625f258a71b9cff0ab701
Robert Lauv <robert.lauv3@gmail.com>
2023-06-19 17:41:10 -0700

    Added rough contacts bar

---
+++ src/components/contacts.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import '../styles/contactsBar/contactsBar.css';
+
+import linkedin_icon from '../assets/svgs/linkedin.svg'
+import github_icon from '../assets/svgs/github-mark.svg';
+import pdf_icon from '../assets/imgs/adobe_pdf.png'
+import resume from '../assets/files/resume.pdf'
+
+class Contacts extends Component {
+
+    render() {
+        return(
+            <div class="bottom-icons">
+                <div id="contact">
+                    <ul class="bottom-icons">
+                        {/* LinkedIn */}
+                        <li class="bottom-icons">
+                            <a target="_blank" href="https://www.linkedin.com/in/robertlauv/">
+                                <img class="logo" alt="LinkedIn logo" src={linkedin_icon}></img>
+                                {/* https://brand.linkedin.com/policies */}
+                            </a>
+                        </li>
+                        {/* Github */}
+                        <li class="bottom-icons">
+                            <a target="_blank" href="https://github.com/rlmech">
+                                <img class="logo" alt="GitHub logo" src={github_icon}></img>
+                                {/* https://github.com/logos */}
+                            </a>
+                        </li>
+                        {/* Resume */}
+                        <li class="bottom-icons">
+                            <a target="_blank" href={resume}>
+                                <img class="logo" alt="pdf file img" src={pdf_icon}></img>
+                                {/* https://www.adobe.com/legal/permissions/icons-web-logos.html */}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Contacts;
---
+++ src/components/tab.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Tab extends Component {
+
+    styles = {
+        
+    }
+
+    render(){
+        return (
+            <h1>Test tab</h1>
+        );
+    }
+}
+
+export default Tab;
