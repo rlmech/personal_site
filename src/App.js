dbeac6f42c4ffe3af180cc270f633ecae72db347
Robert Lauv <robert.lauv3@gmail.com>
2023-07-02 18:21:24 -0700

    Reordered components to match welcome page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 
 import { Welcome, About, Experience, Projects, Contacts } from './containers';
-import { Navbar, Button, Page, Tab } from './components';
+import { Navbar, Button, Tab } from './components';
 
 function App() {
   return (
     <div className="App">
       <Welcome />
       <About />
-      <Experience />
       <Projects />
+      <Experience />
       <Contacts />
     </div>
   );
