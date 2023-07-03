7c0ffad7df73f1b88e0ac62d88f7899820f54d82
Robert Lauv <robert.lauv3@gmail.com>
2023-07-02 18:23:05 -0700

    Changed file to reflect page component change

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Button } from './button/button';
-export { default as Page } from './page/page';
 export { default as Tab } from './tab/tab';
 export { default as Navbar } from './navbar/navbar';
