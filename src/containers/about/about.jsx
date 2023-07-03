19bc710d9f9ac4452a43522ddb7e0fa1fcf1b4da
Robert Lauv <robert.lauv3@gmail.com>
2023-07-02 18:23:42 -0700

    Added About page

--- src/containers/about/about.jsx
+++ src/containers/about/about.jsx
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import './about.css'
+import '../general-styles/page.css'
+import Peter from '../../assets/imgs/BCeater-right.png'
+import Robert from '../../assets/imgs/robert_img.jpg'
 
 const About = () => {
     return (
-        <div className="about-screen">
-            <div className="about-fg">
-                <h1>About</h1>
+        <div className="page-screen-background" id="about">
+            <div className="page-foreground">
+                <div className="page-title">
+                    <p>About</p>
+                </div>
+                <div className="about-text">
+                    <p>My name is Robert Lauv. 
+                        I'm currently a third year pursuing a B.S. in Computer Science at University of California, Irvine.</p>
+                    <br></br>
+                    <p>My interests are somewhere.</p>
+                    <br></br>
+                    <p>My pastimes include eating, hanging out with friends, asking random questions about life, 
+                        and playing games like Chess, Valorant, Terraria, etc.</p>
+                </div>
+                <img src = {Peter} className="petr_img"></img>
+                <div className="robert_img">
+                    <img src = {Robert}></img>
+                </div>
             </div>
         </div>
     );
