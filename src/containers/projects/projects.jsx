7c0103f32fe3dc9ce456df9242758d32be1cd9f4
Robert Lauv <robert.lauv3@gmail.com>
2023-07-03 00:37:00 -0700

    Added simple projects page

--- src/containers/projects/projects.jsx
+++ src/containers/projects/projects.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './projects.css'
+import PersonalSite from '../../assets/imgs/project_personal_site.jpg'
 
 const Projects = () => {
     return (
@@ -7,6 +8,30 @@ const Projects = () => {
             <div className="page-foreground">
                 <div className="page-title">
                     <p>Projects</p>
+                </div>
+                <div className="projects-showcase">
+                    <ul className="projects-list">
+                        <li>
+                            <div>
+                                <a href={'https://github.com/rlmech/personal_site'} target='_blank'><img src={PersonalSite}></img></a>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <p>Project 2</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <p>Project 3</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <p>Project 4</p>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
