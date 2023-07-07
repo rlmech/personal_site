fd5c42ec3004ffc5077982e22300e9af2725b1c4
Robert Lauv <robert.lauv3@gmail.com>
2023-07-06 17:34:15 -0700

    Somewhat animated navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './navbar.css'
 import { Link } from 'react-router-dom';
 
+
+
+
 function Navbar () {
 
     const [navActive, setNavActive] = React.useState(false);
@@ -9,20 +12,24 @@ function Navbar () {
         setNavActive(!navActive);
     }
 
+    const pages = ["", "about", "projects", "experience", "contacts"];
+    const insertNavItems = pages.map((item) => {
+        const pageurl = '/' + item;
+        if ( item === "" ){
+            return <li>{(navActive) && (<Link to={pageurl}>HOME</Link>)}</li>
+        } else {
+            return <li>{(navActive) && (<Link to={pageurl}>{item.toUpperCase()}</Link>)}</li>
+        }
+    });
+
     return (
         <nav className="navbar">
             <div className="navbar-icon" onClick={toggleActive}>
                 <p>&lt;&lt;</p>
             </div>
-            {(navActive) && (
-            <ul className="navbar-list">
-                <li><Link to="/">HOME</Link></li>
-                <li><Link to="/about">ABOUT</Link></li>
-                <li><Link to="/projects">PROJECTS</Link></li>
-                <li><Link to="/experience">EXPERIENCE</Link></li>
-                <li><Link to="/contacts">CONTACT</Link></li>
+            <ul className={`navbar-list ${navActive ? 'navbar-list-active' : null}`}>
+                {insertNavItems}
             </ul>
-            )}
         </nav>
     );
 
